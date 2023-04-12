7706e008e27ce15da7941bdad0a6ec1ab27c05aa
nadine525 <117771680+nadine525@users.noreply.github.com>
2023-04-12 08:16:11 +0000

    Deploying to gh-pages from @ nadine525/goit-js-hw-09@93fe08a650e5980ac74093c0b42cead4bbed7516 🚀

---
+++ 01-color-switcher.29f82368.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let a=null;e.disabled=!0,t.addEventListener("click",(()=>{a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.29f82368.js.map

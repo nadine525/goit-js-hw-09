e74e574b21447a17b3248db8e0f4cb798c1e507d
nadine525 <02_seeders_wide@icloud.com>
2023-04-12 20:39:50 +0200

    second task

---
+++ src/js/02-timer.js
@@ -0,0 +1,76 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import Notiflix from 'notiflix';
+
+const input = document.querySelector('#datetime-picker');
+const button = document.querySelector('button[data-start]');
+const dataDays = document.querySelector('span[data-days]');
+const dataHours = document.querySelector('span[data-hours]');
+const dataMinutes = document.querySelector('span[data-minutes]');
+const dataSeconds = document.querySelector('span[data-seconds]');
+
+button.disabled = true;
+
+const option = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate < currentDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      button.disabled = false;
+    }
+  },
+};
+
+flatpickr(input, option);
+
+let counterId = null;
+
+const onClick = () => {
+  counterId = setInterval(() => {
+    const futureDate = new Date(input.value);
+    const presentDate = Date.now();
+
+    const counterTime = convertMs(futureDate - presentDate);
+    // console.log(counterTime);
+    changeCounter(counterTime);
+
+    if (counterTime < 0) {
+      clearInterval(counterId);
+    }
+  }, 1000);
+
+  button.disabled = true;
+};
+
+button.addEventListener('click', onClick);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function changeCounter({ days, hours, minutes, seconds }) {
+  dataDays.textContent = addLeadingZero(days);
+  dataHours.textContent = addLeadingZero(hours);
+  dataMinutes.textContent = addLeadingZero(minutes);
+  dataSeconds.textContent = addLeadingZero(seconds);
+}
